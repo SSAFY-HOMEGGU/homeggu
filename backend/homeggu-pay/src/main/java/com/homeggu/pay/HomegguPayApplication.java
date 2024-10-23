c111ee1232e066fd6ab3ddbd288fff51cb342e4a
이규빈 <kyubin.dev@gmail.com>
2024-10-23 14:45:52 +0900

    init: pay 서비스 초기 설정

---
+++ backend/homeggu-pay/src/main/java/com/homeggu/pay/HomegguPayApplication.java
@@ -0,0 +1,13 @@
+package com.homeggu.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomegguPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomegguPayApplication.class, args);
+	}
+
+}
