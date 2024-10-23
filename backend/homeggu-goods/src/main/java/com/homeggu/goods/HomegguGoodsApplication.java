757e976eaf57d0758ee46af1c46eba4280487bee
BigCuteMan <hcahna789@naver.com>
2024-10-23 13:33:22 +0900

    init: homeggu-goods proeject
    homeggu-goods proeject init

---
+++ backend/homeggu-goods/src/main/java/com/homeggu/goods/HomegguGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.homeggu.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomegguGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomegguGoodsApplication.class, args);
+	}
+
+}
