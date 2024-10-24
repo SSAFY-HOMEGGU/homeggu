425a26b227cb5d8b39dfca5dacba2961e9753b33
YunSeok-Choi <dbstjr9898@naver.com>
2024-10-25 00:00:01 +0900

    be_fix: 메시지 전송 오류 수정

--- backend/homeggu-chat/src/main/java/com/homeggu/chat/global/config/WebSocketConfig.java
+++ backend/homeggu-chat/src/main/java/com/homeggu/chat/global/config/WebSocketConfig.java
@@ -1,19 +1,23 @@
 package com.homeggu.chat.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Value("${spring.rabbitmq.host}")
     private String rabbitmqHost;
 
-    @Value("${spring.rabbitmq.port}")
-    private int rabbitmqPort;
+    @Value("${spring.stomp.port}")
+    private int stompPort;
 
     @Value("${spring.rabbitmq.username}")
     private String rabbitmqUsername;
@@ -26,7 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub") // 발행
                 .enableStompBrokerRelay("/exchange") // 구독
                 .setRelayHost(rabbitmqHost)
-                .setRelayPort(rabbitmqPort)
+                .setRelayPort(stompPort)
                 .setClientLogin(rabbitmqUsername)
                 .setClientPasscode(rabbitmqPassword);
     }
@@ -34,7 +38,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+//                .setAllowedOrigins("*")
                 .setAllowedOriginPatterns("*")
 //                .setHandshakeHandler(webSocketHandShakeHandler)
 //                .addInterceptors(customHttpSessionHandshakeInterceptor())
