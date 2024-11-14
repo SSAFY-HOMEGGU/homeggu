42fdf5807adf444be0b895f5113bb49ba6250883
Yungsix <dbstjr9898@naver.com>
2024-11-14 11:22:38 +0900

    be_chore: 각 서비스 테스트 파일 배포 설정 추가

--- backend/homeggu-gateway/src/test/java/com/homeggu/gateway/HomegguGatewayApplicationTests.java
+++ backend/homeggu-gateway/src/test/java/com/homeggu/gateway/HomegguGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.homeggu.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Test.class)
 class HomegguGatewayApplicationTests {
 
     @Test
