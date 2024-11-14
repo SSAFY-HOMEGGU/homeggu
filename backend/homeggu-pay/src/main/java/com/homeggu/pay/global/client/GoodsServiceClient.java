059d60f1a1b1f657f5002f8fba720053597fd3d3
이규빈 <kyubin.dev@gmail.com>
2024-11-14 15:20:32 +0900

    feat: FeignClient를 ã이용해 goods 서비스에서 pay 서비스로 데이터 보내는 로직 추가

---
+++ backend/homeggu-pay/src/main/java/com/homeggu/pay/global/client/GoodsServiceClient.java
@@ -0,0 +1,14 @@
+package com.homeggu.pay.global.client;
+
+import com.homeggu.pay.global.client.dto.response.SalesBoardResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "homeggu-goods")
+public interface GoodsServiceClient {
+
+    @GetMapping("/goods/board/{boardId}")
+    SalesBoardResponse getGoods(@PathVariable("boardId") Long boardId);
+
+}
