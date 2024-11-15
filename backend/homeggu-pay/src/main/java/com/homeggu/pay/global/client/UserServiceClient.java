f131c58921b9dfc802d8301464253e343d243c7d
이규빈 <kyubin.dev@gmail.com>
2024-11-15 10:03:09 +0900

    fix : UserServiceClient 수정

--- backend/homeggu-pay/src/main/java/com/homeggu/pay/global/client/UserServiceClient.java
+++ backend/homeggu-pay/src/main/java/com/homeggu/pay/global/client/UserServiceClient.java
@@ -3,12 +3,12 @@ package com.homeggu.pay.global.client;
 import com.homeggu.pay.global.client.dto.response.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "homeggu-user")
 public interface UserServiceClient {
 
     @GetMapping("/user/profile/detail")
-    UserResponse getUserProfile(@RequestParam("userId") Long userId);
+    UserResponse getUserProfile(@RequestHeader("userId") Long userId);
 
 }
