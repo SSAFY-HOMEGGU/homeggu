fb0d2cb9444407acc654e0a305508020ed1e887b
sunmin <bbu0330@gmail.com>
2024-11-15 09:05:25 +0900

    fix: 빌드 수정중

--- frontend/src/app/(pages)/interior/page.js
+++ frontend/src/app/(pages)/interior/page.js
@@ -1,53 +1,60 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { useReactPlanner } from "./hooks/useReactPlanner";
-import { Provider } from "react-redux";
-import dynamic from "next/dynamic";
+// import React from "react";
+// import { useReactPlanner } from "./hooks/useReactPlanner";
+// import { Provider } from "react-redux";
+// import dynamic from "next/dynamic";
 
-const ReactPlanner = dynamic(
-  () => import("../../../../react-planner/es/ReactPlanner"),
-  {
-    ssr: false,
-    loading: () => <div>Loading planner components...</div>,
-  }
-);
+// const ReactPlanner = dynamic(
+//   () => import("../../../../react-planner/es/ReactPlanner"),
+//   {
+//     ssr: false,
+//     loading: () => <div>Loading planner components...</div>,
+//   }
+// );
 
-const InteriorPage = () => {
-  const { components, windowSize, stateExtractor, isReady } = useReactPlanner();
-  const [mounted, setMounted] = React.useState(false);
+// const InteriorPage = () => {
+//   const { components, windowSize, stateExtractor, isReady } = useReactPlanner();
+//   const [mounted, setMounted] = React.useState(false);
 
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
+//   React.useEffect(() => {
+//     setMounted(true);
+//   }, []);
 
-  if (!mounted) {
-    return <div>Loading...</div>;
-  }
+//   if (!mounted) {
+//     return <div>Loading...</div>;
+//   }
 
-  if (!isReady || !components) {
-    return <div>Initializing planner components...</div>;
-  }
+//   if (!isReady || !components) {
+//     return <div>Initializing planner components...</div>;
+//   }
 
-  console.log("Rendering InteriorPage with components:", !!components);
+//   console.log("Rendering InteriorPage with components:", !!components);
 
+//   return (
+//     <Provider store={components.store}>
+//       <div style={{ height: "100vh" }}>
+//         <ReactPlanner
+//           catalog={components.catalog}
+//           width={windowSize.width}
+//           height={windowSize.height - 50}
+//           translator={components.translator}
+//           stateExtractor={stateExtractor}
+//           plugins={[]}
+//           customContents={components.customContents}
+//           toolbarButtons={[]}
+//           sidebarComponents={[]}
+//         />
+//       </div>
+//     </Provider>
+//   );
+// };
+
+// export default InteriorPage;
+import React from 'react'
+
+export default function page() {
   return (
-    <Provider store={components.store}>
-      <div style={{ height: "100vh" }}>
-        <ReactPlanner
-          catalog={components.catalog}
-          width={windowSize.width}
-          height={windowSize.height - 50}
-          translator={components.translator}
-          stateExtractor={stateExtractor}
-          plugins={[]}
-          customContents={components.customContents}
-          toolbarButtons={[]}
-          sidebarComponents={[]}
-        />
-      </div>
-    </Provider>
-  );
-};
-
-export default InteriorPage;
+    <div>page</div>
+  )
+}
