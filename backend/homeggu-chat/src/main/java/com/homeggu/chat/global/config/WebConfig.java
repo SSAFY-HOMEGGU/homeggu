3a0f3b1cfdf4436acdc6e379d5aa963861832196
YunSeok-Choi <dbstjr9898@naver.com>
2024-11-16 03:30:00 +0900

    be_fix: chat server cors 수정

--- backend/homeggu-chat/src/main/java/com/homeggu/chat/global/config/WebConfig.java
+++ backend/homeggu-chat/src/main/java/com/homeggu/chat/global/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 허용할 출처
+                .allowedOrigins("https://k11b206.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(60); // Options 요청을 보내지 않는 시간
