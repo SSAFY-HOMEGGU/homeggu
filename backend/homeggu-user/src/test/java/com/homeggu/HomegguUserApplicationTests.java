858d6493e0947296414055b5802cc675e72716e3
YunSeok-Choi <dbstjr9898@naver.com>
2024-11-16 19:05:45 +0900

    be_fix: test contextload 수정

--- backend/homeggu-user/src/test/java/com/homeggu/HomegguUserApplicationTests.java
+++ backend/homeggu-user/src/test/java/com/homeggu/HomegguUserApplicationTests.java
@@ -3,7 +3,7 @@ package com.homeggu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = Test.class)
+// @SpringBootTest(classes = Test.class)
 class HomegguUserApplicationTests {
 
     @Test
