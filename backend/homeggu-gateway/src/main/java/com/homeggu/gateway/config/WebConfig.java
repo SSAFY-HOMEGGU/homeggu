4e422bc4625cba0808e11754be5dbdafab471e50
taeseong <tskim4065@gmail.com>
2024-11-18 00:19:36 +0900

    fix: 원복

--- backend/homeggu-gateway/src/main/java/com/homeggu/gateway/config/WebConfig.java
+++ backend/homeggu-gateway/src/main/java/com/homeggu/gateway/config/WebConfig.java
@@ -2,23 +2,33 @@ package com.homeggu.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(60); // Options 요청을 보내지 않는 시간
-    }
+@EnableWebFlux
+public class WebConfig {
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setMaxAge(60L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+
+        @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
--- backend/homeggu-gateway/src/main/java/com/homeggu/gateway/filter/JwtFilter.java
+++ backend/homeggu-gateway/src/main/java/com/homeggu/gateway/filter/JwtFilter.java
@@ -2,34 +2,30 @@ package com.homeggu.gateway.filter;
 
 import com.homeggu.gateway.jwt.JwtProvider;
 import io.jsonwebtoken.Claims;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
 import java.util.Collections;
 
 @RequiredArgsConstructor
-public class JwtFilter extends OncePerRequestFilter {
+public class JwtFilter implements WebFilter {
 
     private final JwtProvider jwtProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         // 1. 요청 헤더에서 "Authorization"을 가져옴
-        String authorizationHeader = request.getHeader("Authorization");
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
 
         // 2. 헤더가 비어 있거나 "Bearer"로 시작하지 않으면 필터 체인
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
+            return chain.filter(exchange);
         }
 
         // 3. "Bearer" 이후의 토큰만 추출
@@ -39,17 +35,25 @@ public class JwtFilter extends OncePerRequestFilter {
         if (jwtProvider.validateToken(token)) {
             // 5. 유효한 경우 토큰에서 사용자 정보 추출
             Claims claims = jwtProvider.parseToken(token);
-            int userId = claims.get("userId", Integer.class);
+            Long userId = claims.get("userId", Long.class);
 
             // 6. 사용자 인증 객체 생성 후 SecurityContext에 설정
             Authentication authentication = new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            // 7. userId를 응답 헤더에 추가
-            response.addHeader("userId", String.valueOf(userId));
+            // 7. userId를 custom header로 추가
+//            ServerWebExchange modifiedExchange = exchange.mutate()
+//                    .request(r -> r.header("userId", String.valueOf(userId)))
+//                    .build();
+
+            // 수정 - 헤더를 직접 수정하는 대신 userId 속성을 추가
+            exchange.getAttributes().put("userId", String.valueOf(userId));
+
+            // 8. userId가 추가된 헤더와 함께 다음 필터로 요청 전달
+            return chain.filter(exchange);
         }
 
-        // 8. 다음 필터로 요청 전달
-        filterChain.doFilter(request, response);
+        // 9. 토큰이 유효하지 않은 경우 필터 체인 유지
+        return chain.filter(exchange);
     }
 }
