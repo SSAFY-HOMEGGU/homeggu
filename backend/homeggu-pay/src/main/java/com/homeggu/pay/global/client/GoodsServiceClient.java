57f91ed4bc85d348ea030760baed5d8a9647b6f8
이규빈 <kyubin.dev@gmail.com>
2024-11-18 12:42:42 +0900

    fix: feignClient url 수정

--- backend/homeggu-pay/src/main/java/com/homeggu/pay/global/client/GoodsServiceClient.java
+++ backend/homeggu-pay/src/main/java/com/homeggu/pay/global/client/GoodsServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "homeggu-goods")
+@FeignClient(name = "homeggu-goods", url = "https://k11b206.p.ssafy.io")
 public interface GoodsServiceClient {
 
     @GetMapping("/api/goods/board/{boardId}")
