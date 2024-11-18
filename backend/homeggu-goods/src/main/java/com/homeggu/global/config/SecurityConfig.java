9b11224b81e9a3c5cfbc3d1dcefceebe8ca1fb8a
YunSeok-Choi <dbstjr9898@naver.com>
2024-11-19 01:35:36 +0900

    be_fix: goods security 수정

--- backend/homeggu-goods/src/main/java/com/homeggu/global/config/SecurityConfig.java
+++ backend/homeggu-goods/src/main/java/com/homeggu/global/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.homeggu.global.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.web.cors.reactive.CorsConfigurationSource;
 
 //@Configuration
 //@EnableWebFluxSecurity
